3ac5884b22c828c631e8de716f969b8b208a9976
ollilaunonen <82574570+ollilaunonen@users.noreply.github.com>
2022-02-20 22:28:15 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Mikä on tietokone?",
+    answer: "Laite joka käsittelee numeris-loogista tietoa ohjelmointinsa mukaisesti",
+    options: [
+      "Laite joka käsittelee numeris-loogista tietoa ohjelmointinsa mukaisesti",
+      "Laite, joka pesee pyykkiä",
+      "Kone, jolla voi tehdä kirjoja",
+      "Kone, joka valmistaa sukkia"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Mitä osia tarvitaan pelikoneeseen?",
+    answer: "Suoritin, emolevy, näytönohjain, keskusmuisti, kiintolevy, verkkokortti, internetyhteys, virtalähde",
+    options: [
+      "Suoritin, emolevy, näytönohjain, keskusmuisti, kiintolevy, verkkokortti, internetyhteys, virtalähde",
+      "Piirilevy, kamera, vhs-nauhuri, virtanäppäin, hiirimatto",
+      "Lautanen, haarukka, kirja, antenni, jatkojohto",
+      "Pelipöytä, hiiri, penaali, näppäimistö, usb-tikku"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Mikä on näytönohjain?",
+    answer: "Komponentti, joka piirtää grafiikan tietokoneen näytölle",
+    options: [
+      "Näyttö, joka piirtää grafiikan näytönohjaimelle",
+      "Komponentti, joka piirtää grafiikan tietokoneen näytölle",
+      "Näyttöön kytketty näppäimistöllinen peliohjain",
+      "Laite, joka puhdistaa näytön likaantumiselta"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Mikä on suoritin(prosessori)",
+    answer: "Tietokoneen osa, joka suorittaa tietokoneohjelman sisältämiä konekielisiä käskyjä",
+    options: [
+      "Tietokoneen osa, joka suorittaa tietokoneohjelman sisältämiä konekielisiä käskyjä",
+      "Tietokoneen ulkopuolinen virtalähde",
+      "Mukana kannettava usb-tikku",
+      "Laite, joka mittaa pulssin ranteesta"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Mikä on emolevy? ",
+    answer: "Tietokoneen keskeinen piirilevy, johon tietokoneen muut osat kiinnitetään",
+    options: [
+      "levy, jossa on emon ominaisuudet",
+      "suklaalevy",
+      "urheiluväline",
+      "Tietokoneen keskeinen piirilevy, johon tietokoneen muut osat kiinnitetään"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+    numb: 6,
+    question: "Mikä on virtalähteen tehtävä tietokoneessa?",
+     answer: "Ylläpitää tietokoneen toiminnassa vaadittavaa virtaa ja jännitettä",
+     options: [
+       "Näyttää kuvaa näytössä",
+       "Täydentää jäähdytysvarastoa",
+      "Ylläpitää tietokoneen toiminnassa vaadittavaa virtaa ja jännitettä",
+       "Usb-väylän protokolla"
+     ]
+   },
+];
